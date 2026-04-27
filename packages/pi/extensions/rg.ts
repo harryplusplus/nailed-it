8e4aaae1efcd21ff4aee833a5b4111d5fc856145
harryplusplus <harryplusplus@gmail.com>
2026-04-28 01:46:26 +0900

    feat(rg-extension): add ripgrep pipe escaping guideline to system prompt

--- packages/pi/extensions/rg.ts
+++ packages/pi/extensions/rg.ts
@@ -13,8 +13,17 @@ export default async function (pi: ExtensionAPI) {
   })
 
   pi.on('before_agent_start', async event => {
-    return {
-      systemPrompt: event.systemPrompt + '\n\nUse `rg` instead of `grep`.',
-    }
+    const lines = [
+      '',
+      '',
+      'Always use `rg` instead of `grep`.',
+      '',
+      'When using `rg`, keep the following rules in mind. In `rg` patterns, the pipe character `|` is an alternation (OR) operator. Do NOT escape it with a backslash. For example, write `rg "foo|bar" file.ts`, not `rg "foo\\|bar" file.ts`. The latter searches for the literal string "foo|bar" and will not match either "foo" or "bar".',
+      '',
+      'Examples:',
+      '1. To find either `isStreaming` or `agent_end`, use `rg "isStreaming|agent_end" file.js`.',
+      "2. To match a literal backslash-pipe (`\\|`), use a character class: `rg '[\\|]' file.js`.",
+    ]
+    return { systemPrompt: event.systemPrompt + lines.join('\n') }
   })
 }
